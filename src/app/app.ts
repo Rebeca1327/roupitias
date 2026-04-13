d8eac4149f4268108389167940d5b662ba8b7c11
Rebeca1327 <rebecagmatos2006@gmail.com>
2026-04-13 16:34:28 -0500

    umas roupa ai

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Menu} from './menu/menu'
+import {Cards} from './cards/cards'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Menu, Cards ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('loja-de-roupas');
+}
